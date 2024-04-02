587306e93d88f115906436cea5892b2ba6cc396a
Juan Vega <jjvegam@uni.pe>
2024-04-02 15:32:22 -0500

    act

--- bundle.js
+++ bundle.js
@@ -1,1 +1,16 @@
-console.log("hola mundo 2.0");
+const button = document.getElementById("input-button");
+const input = document.getElementById("input-element");
+console.log("button");
+button.onclick = () => {
+    input.click();
+}; 
+
+input.onchange = (changed) => {
+    const reader = new FileReader();
+        reader.onload = () => LoadIfc(reader.result);
+        reader.readAsText(changed.target.files[0]);
+};
+
+function LoadIfc(ifcData) {
+    console.log(ifcData);
+}
